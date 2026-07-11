9eed15712b95c864b430f79504ceafaeff6ab957
T3lKing01 <joshuamambozindoga03@gmail.com>
2026-07-11 20:47:29 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/**
+* Leader of the Youth
+* LIGHT DESK TEAM dey guide 
+*/
+
+const settings = {
+  packname: 'ᴍɪᴅᴋɪɴɢ ᴛᴇᴄʜ',
+  author: 'NetKing',
+  botName: "𝗖𝗥𝗔𝗭𝗬 𝗫",
+  botOwner: 'ᴍɪᴅᴋɪɴɢ', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '263716985350', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.7.1",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/N3tKing01/unodeyipano/archive/refs/heads/main.zip",
+  timezone: 'Africa/Harare',
+};
+
+module.exports = settings;
